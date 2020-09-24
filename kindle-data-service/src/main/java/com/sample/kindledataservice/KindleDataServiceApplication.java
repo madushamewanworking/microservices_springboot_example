bfc4feb5171cf7573bceb8276d0850e84f3e7af6
madushamewanworking <madushamewan.working@gmail.com>
2020-09-24 08:26:04 +0530

    init kindle-data-service module

---
+++ kindle-data-service/src/main/java/com/sample/kindledataservice/KindleDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sample.kindledataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindleDataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KindleDataServiceApplication.class, args);
+    }
+
+}
---
+++ kindle-data-service/src/test/java/com/sample/kindledataservice/KindleDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.kindledataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KindleDataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
