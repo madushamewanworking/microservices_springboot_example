2f21dbc492282ce7e82e0a61b055d5923be0f3e3
madushamewanworking <madushamewan.working@gmail.com>
2020-09-24 08:39:18 +0530

    init book-ratinginfo-service module

---
+++ book-ratinginfo-service/src/main/java/com/sample/bookratinginfoservice/BookRatinginfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sample.bookratinginfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRatinginfoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookRatinginfoServiceApplication.class, args);
+    }
+
+}
---
+++ book-ratinginfo-service/src/test/java/com/sample/bookratinginfoservice/BookRatinginfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.bookratinginfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRatinginfoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
